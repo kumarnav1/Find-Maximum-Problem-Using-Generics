132da21eef74286c4eea04f6e601535506c837b7
nk400 <navkr007@gmail.com>
2022-05-31 04:04:40 +0530

    ADDED: Welcome message

---
+++ src/com/bl/find_maximum_using_generics/FindMaximum.java
@@ -0,0 +1,9 @@
+package com.bl.find_maximum_using_generics;
+
+public class FindMaximum {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Find maximum problem using Generics.");
+    }
+}
